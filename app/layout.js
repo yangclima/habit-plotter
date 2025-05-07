6a9a4704fbd5f70f7c26a39e2e68f26684fe9217
yangclima <yangclima@gmail.com>
2025-05-06 23:33:42 -0300

    chore: Create basic project structure with next.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Habit Plotter",
+  description: "🌱 Small steps, big patterns — track and grow.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
